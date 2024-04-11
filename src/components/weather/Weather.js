1c94e2b764462deb5570ab9b0b6bcb023240b02c
toska27 <nemanjatoskic58@gmail.com>
2024-04-11 17:57:54 +0200

    bg-image for different weather

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -3,15 +3,36 @@ import Search from "../search/Search.js";
 import Info from "../info/Info.js";
 import "./Weather.css";
 
-const Weather = () => {
+const Weather = ({ onChange }) => {
   const [city, setCity] = useState({});
 
   const handleCityChange = (newCity) => {
     setCity(newCity);
+    onChange(newCity);
   };
 
+  let divName = "weather";
+  if (city.weather) {
+    if (city.weather[0].description === "mist") {
+      divName = "weather mist";
+    } else if (
+      city.weather[0].description === "broken clouds" ||
+      city.weather[0].description === "scattered sky"
+    ) {
+      divName = "weather broken";
+    } else if (city.weather[0].description === "clear sky") {
+      divName = "weather clear";
+    } else if (city.weather[0].description === "overcast clouds") {
+      divName = "weather overcast";
+    } else if (city.weather[0].description === "few clouds") {
+      divName = "weather few";
+    } else if (city.weather[0].description === "haze") {
+      divName = "weather haze";
+    }
+  }
+
   return (
-    <div className="weather">
+    <div className={divName}>
       <Search onChange={handleCityChange} />
       <Info city={city} />
     </div>
